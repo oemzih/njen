4f09f47ff8f1a1535f68e338fdb697d15932fde2
oemzih <104249389+oemzih@users.noreply.github.com>
2025-09-24 08:24:34 +0700

    Create expair.lua

---
+++ expair.lua
@@ -0,0 +1,8 @@
+-- File: expair.lua
+
+-- Tanggal kadaluarsa dalam format "YYYY-MM-DD"
+-- Contoh: "2025-09-25" berarti kadaluarsa pada akhir 25 September 2025
+
+return {
+    ["qweasd"] = "2025-09-25",
+}
