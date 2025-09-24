653918ff8f3e494a0bbf7cebf5e8c5ecbb89bd7a
oemzih <104249389+oemzih@users.noreply.github.com>
2025-09-24 16:37:22 +0700

    Update expair.lua

--- expair.lua
+++ expair.lua
@@ -3,6 +3,10 @@
 -- Tanggal kadaluarsa dalam format "YYYY-MM-DD"
 -- Contoh: "2025-09-25" berarti kadaluarsa pada akhir 25 September 2025
 
+--return {
+   -- ["sehari"] = "2025-09-25",
+--}
 return {
-    ["sehari"] = "2025-09-25",
+    ["keyanda"] = "2025-09-25",
+    ["keylain"] = "2026-01-01"
 }
